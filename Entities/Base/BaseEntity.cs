176acd63af6e1b0ebe40607e78330080d40fe4a9
d.sepid <S.Dalirpour@samaneh.co>
2020-11-29 18:28:57 +0330

    Add BaseEntity Class

---
+++ Entities/Base/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public abstract class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+
+    public abstract class BaseEntity : BaseEntity<int>
+    {
+       
+    }
+}
